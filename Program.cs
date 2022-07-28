6aadfb3dd922a715528928ab081f06c78c2dc8e9
SANLAB\ismail.turkmenoglu <ismail.turkmenoglu@sanlab.net>
2022-07-28 15:51:10 +0300

    Init

--- Program.cs
+++ Program.cs
@@ -16,6 +16,9 @@ namespace BookManagement
             CreateHostBuilder(args).Build().Run();
         }
 
+
+        
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
